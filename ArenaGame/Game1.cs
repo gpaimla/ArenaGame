9b1c39ce18e84966d93000d155223760754ec422
burn1nsun <burn1nsunn@gmail.com>
2017-04-22 23:12:40 +0300

    Initializing game loop logic, walking, animation, window sizing

--- ArenaGame/Game1.cs
+++ ArenaGame/Game1.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.Xna.Framework;
+﻿#region Using Statements
+
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace ArenaGame
+#endregion
+
+namespace WalkingGame
 {
     /// <summary>
     /// This is the main type for your game.
@@ -12,10 +16,22 @@ namespace ArenaGame
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        CharacterEntity character;
+        KeyboardState keyBoardState = Keyboard.GetState();
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width; // set this value to the desired width of your window
+            graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;  // set this value to the desired height of your window
+            graphics.IsFullScreen = true;
+            
+            graphics.ApplyChanges();
+
             Content.RootDirectory = "Content";
+            graphics.IsFullScreen = true;
+
+            
         }
 
         /// <summary>
@@ -26,9 +42,10 @@ namespace ArenaGame
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            character = new CharacterEntity(this.GraphicsDevice);
 
             base.Initialize();
+
         }
 
         /// <summary>
@@ -40,16 +57,6 @@ namespace ArenaGame
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
-        }
-
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
-        protected override void UnloadContent()
-        {
-            // TODO: Unload any non ContentManager content here
         }
 
         /// <summary>
@@ -59,12 +66,32 @@ namespace ArenaGame
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            // TODO: Add your update logic here
-
+            character.Update(gameTime);
             base.Update(gameTime);
+
+            //if (keyBoardState.IsKeyDown(Keys.F11))
+            //{
+            //    this.graphics.IsFullScreen = true;
+            //    this.graphics.ToggleFullScreen();
+            //    this.graphics.ApplyChanges();
+            //}
+
+                if (Keyboard.GetState().IsKeyDown(Keys.F11))
+                {
+                    if (graphics.IsFullScreen)
+                    {
+                        graphics.PreferredBackBufferWidth = 1280;
+                        graphics.PreferredBackBufferHeight = 720;
+                    }
+                    else
+                    {
+                        graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width; // set this value to the desired width of your window
+                        graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+                    }
+                    graphics.ToggleFullScreen();
+                }
+                graphics.ApplyChanges();
+            
         }
 
         /// <summary>
@@ -75,7 +102,13 @@ namespace ArenaGame
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            // We'll start all of our drawing here:
+            spriteBatch.Begin();
+
+            // Now we can do any entity rendering:
+            character.Draw(spriteBatch);
+            // End renders all sprites to the screen:
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
