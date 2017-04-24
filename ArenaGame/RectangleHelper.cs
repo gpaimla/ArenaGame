8fcb7a8a48018db88439900f82d56d2763f28bc2
burn1nsun <burn1nsunn@gmail.com>
2017-04-24 17:59:18 +0300

    RectangleHelperRevertChanges

--- ArenaGame/RectangleHelper.cs
+++ ArenaGame/RectangleHelper.cs
@@ -11,7 +11,7 @@ namespace ArenaGame
     {
         public static bool TouchTopOf(this Rectangle r1, Rectangle r2)
         {
-            return (r1.Bottom >= r2.Top &&
+            return (r1.Bottom >= r2.Top - 2 &&
                     r1.Bottom <= r2.Top + (r2.Height / 2) &&
                     r1.Right >= r2.Left + (r2.Width / 5) &&
                     r1.Left <= r2.Right - (r2.Width / 5));
@@ -19,8 +19,8 @@ namespace ArenaGame
 
         public static bool TouchBottomOf(this Rectangle r1, Rectangle r2)
         {
-            return (r1.Top <= r2.Bottom + (r2.Height / 2) &&
-                    r1.Top >= r2.Bottom   &&
+            return (r1.Top <= r2.Bottom + (r2.Height / 5) &&
+                    r1.Top >= r2.Bottom - 2 &&
                     r1.Right >= r2.Left + (r2.Width / 5) &&
                     r1.Left <= r2.Right - (r2.Width / 5));
         }
@@ -28,7 +28,7 @@ namespace ArenaGame
         public static bool TouchLeftOf(this Rectangle r1, Rectangle r2)
         {
             return (r1.Right <= r2.Right &&
-                    r1.Right >= r2.Left   &&
+                    r1.Right >= r2.Left - 5 &&
                     r1.Top <= r2.Bottom - (r2.Width / 4) &&
                     r1.Bottom >= r2.Top + (r2.Width / 4));
         }
@@ -36,9 +36,9 @@ namespace ArenaGame
         public static bool TouchRightOf(this Rectangle r1, Rectangle r2)
         {
             return (r1.Left >= r2.Left &&
-                    r1.Left <= r2.Right  &&
-                    r1.Top +1 >= r2.Top &&
-                    r1.Top -1 <= r2.Bottom);
+                    r1.Left <= r2.Right + 5 &&
+                    r1.Top <= r2.Bottom - (r2.Width / 4) &&
+                    r1.Bottom >= r2.Top + (r2.Width / 4));
         }
     }
 }
