6a600585636e51dd90082ef71fd92d55425d81a8
sfhsdfasg <GertMorten.Paimla@tptlive.ee>
2017-04-29 02:03:11 +0300

    replacment

--- ArenaGame/CollidableTreeCherry.cs
+++ ArenaGame/CollidableTreeCherry.cs
@@ -13,8 +13,7 @@ namespace ArenaGame
         public CollidableTreeCherry(Texture2D Texture2d, Rectangle PictureRectangle)
         {
             this.Texture2d = Texture2d;
-            this.PictureRectangle = PictureRectangle;
-            this.PictureRectangle = new Rectangle(PictureRectangle.X -140, PictureRectangle.Y - 175, 292, 329);
+            this.PictureRectangle = new Rectangle(PictureRectangle.X - 140, PictureRectangle.Y - 175, 292, 329);
             this.CollisionRectangle = new Rectangle(PictureRectangle.X + 16, PictureRectangle.Y + PictureRectangle.Height / 2 + 61, 53, 15);
 
         }
