38c797ee42b4d857dd6d1162288f47f210868636
burn1nsun <burn1nsunn@gmail.com>
2017-05-07 21:12:34 +0300

    Chests

--- ArenaGame/CollidableObjectClasses/CollidableChestVertical.cs
+++ ArenaGame/CollidableObjectClasses/CollidableChestVertical.cs
@@ -8,8 +8,8 @@ namespace ArenaGame
         public CollidableChestVertical(Texture2D Texture2d, Rectangle PictureRectangle)
         {
             this.Texture2d = Texture2d;
-            this.PictureRectangle = PictureRectangle;
-            this.CollisionRectangle = new Rectangle(PictureRectangle.X + (PictureRectangle.Width) / 2, PictureRectangle.Y + PictureRectangle.Height / 2, 64, 100);
+            this.PictureRectangle = new Rectangle(PictureRectangle.X, PictureRectangle.Y, 64, 100);
+            this.CollisionRectangle = new Rectangle(PictureRectangle.X, PictureRectangle.Y, 64, 100);
         }
     }
 }
